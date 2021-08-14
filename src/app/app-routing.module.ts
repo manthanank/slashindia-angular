f0fbdab541e711fd32fcb45ccf8d669637908bea
manthanank <manthan.ank46@gmail.com>
2021-08-14 22:58:25 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ServicesComponent } from './services/services.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { AboutComponent } from './about/about.component';
-import { LampsComponent } from './lamps/lamps.component';
-import { PrintingsComponent } from './printings/printings.component';
-import { TablesComponent } from './tables/tables.component';
+
 
 const routes: Routes = [
-  {path:'', component : HomeComponent},
   {path:'navbar', component : NavbarComponent},
   {path:'footer', component : FooterComponent},
   {path:'services', component : ServicesComponent},
   {path:'carousel', component : CarouselComponent},
   {path:'about', component : AboutComponent},
-
 ];
 
 @NgModule({
