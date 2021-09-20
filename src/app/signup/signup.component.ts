bc66d3e6b17aee7e61ddf95f74572151bda76647
manthanank <manthan.ank46@gmail.com>
2021-09-20 21:46:07 +0530

    commit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public signupForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname:[''],
+      email:[''],
+      password : [''],
+      mobile : ['']
+    })
   }
-
+  signup(){
+    this.http.post<any>("http://localhost:3000/signupUsers",this.signupForm.value)
+    .subscribe(res=>{
+      alert("Signup Success");
+      this.signupForm.reset();
+      this.router.navigate(['login']);
+    },
+    err=>{
+      alert("Something wrong");
+    })
+  }
 }
